f951127160784c21e7e2bbfd3a322381b6978d71
Diego Rodríguez <diego@maniak.com.mx>
2017-07-04 16:18:47 -0500

    Check if meetup is already frozen, send error

--- app/Http/Controllers/Api/MeetupsController.php
+++ app/Http/Controllers/Api/MeetupsController.php
@@ -53,33 +53,39 @@ class MeetupsController extends ApiController
     public function freeze($id)
     {
         $meetup = Meetup::find($id);
+        
         if($meetup->checkOwner($this->user)){
             
-            foreach($meetup->activity_bucket->quizzes as $quiz){
-                $graded_quiz = GradedQuiz::create([
-                    'quiz_id' => $quiz->id,
-                    'quiz_title' => $quiz->title,
-                    'quiz_description' => $quiz->description,
-                    'quiz_type' => json_encode($quiz->type),
-                    'quiz_grade_level' => $quiz->subject->grade_level->name,
-                    'quiz_subject' => $quiz->subject->name,
-                    'meetup_id' => $meetup->id
-                ]);
-                
-                foreach($quiz->questions as $question){
-                    GradedQuizQuestion::create([
-                        
-                        'question_id' => $question->id,
-                        'question_title' => $question->title,
-                        'question_photo' => $question->getOriginal('photo'),
-                        'question_topic' => $question->topic->name,
-                        'answers' => $question->answers->toJson(),
-                        'graded_quiz_id' => $graded_quiz->id
+            if($meetup->graded_quizzes->isEmpty()){
+                foreach($meetup->activity_bucket->quizzes as $quiz){
+                    $graded_quiz = GradedQuiz::create([
+                        'quiz_id' => $quiz->id,
+                        'quiz_title' => $quiz->title,
+                        'quiz_description' => $quiz->description,
+                        'quiz_type' => json_encode($quiz->type),
+                        'quiz_grade_level' => $quiz->subject->grade_level->name,
+                        'quiz_subject' => $quiz->subject->name,
+                        'meetup_id' => $meetup->id
                     ]);
+        
+                    foreach($quiz->questions as $question){
+                        GradedQuizQuestion::create([
+                
+                            'question_id' => $question->id,
+                            'question_title' => $question->title,
+                            'question_photo' => $question->getOriginal('photo'),
+                            'question_topic' => $question->topic->name,
+                            'answers' => $question->answers->toJson(),
+                            'graded_quiz_id' => $graded_quiz->id
+                        ]);
+                    }
                 }
+    
+                return $this->respond('Meetup froze correctly');
+            }else{
+                return $this->respondWithError('This meetup was already frozen');
             }
             
-            return $this->respond('Meetup froze correctly');
         }else{
             return $this->respondWithError('You Do not have permission to freeze this meetup');
         }
