0fd408e0edb0d47db8bd7f9ced71351e9462525e
Jesus Lizarraga <jesus@maniak.com.mx>
2018-07-04 11:39:33 -0500

    Delete unnecessary data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
-use Brightfox\Models\SatSection;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,9 +15,5 @@ class DatabaseSeeder extends Seeder
         Role::create(['name' => 'admin']);
         Role::create(['name' => 'director']);
         Role::create(['name' => 'instructor']);
-        SatSection::create(['name' => 'Reading Comprehension']);
-        SatSection::create(['name' => 'Writing and Language']);
-        SatSection::create(['name' => 'Math No Calculator']);
-        SatSection::create(['name' => 'Math With Calculator']);
     }
 }
