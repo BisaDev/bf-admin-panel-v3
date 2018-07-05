a9d6e7da11054640db4b971769c22265cda915aa
Jesus Lizarraga <jesus@maniak.com.mx>
2018-07-05 10:00:52 -0500

    Add topic and tags to each question in a quiz's view.

--- resources/views/web/quizzes/show.blade.php
+++ resources/views/web/quizzes/show.blade.php
@@ -33,18 +33,9 @@
 
                 <div class="row">
                     <div class="col-sm-6 m-b-15">
-                        <label>Tags:</label><br>
-                            @foreach($item->tags as $tag)
-                            <span class="label label-primary">{{ $tag->name }}</span>
-                            @endforeach
-                    </div>
-                    <div class="col-sm-6 m-b-15">
-                        <label>Topics:</label>
-                            <ul class="list-inline">
-                                @foreach($item->subject->topics as $topic)
-                                    <li class=" list-inline-item">{{ $topic->name }}</li>
-                                @endforeach
-                            </ul>
+                        @foreach($item->tags as $tag)
+                        <span class="label label-primary">{{ $tag->name }}</span>
+                        @endforeach
                     </div>
                 </div>
 
@@ -71,6 +62,18 @@
                                 @if($question->photo)
                                 <img src="{{ $question->photo }}" class="img-responsive thumbnail m-t-5">
                                 @endif
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <label class="control-label">Topic:</label>
+                                        <span> {{ $question->topic->name }}</span>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <label class="control-label">Tags:</label>
+                                        @foreach($question->tags as $tag)
+                                            <span class="label label-primary">{{ $tag->name }}</span>
+                                        @endforeach
+                                    </div>
+                                </div>
                                 <div class="row answer-list m-t-10">
                                     @foreach($question->answers as $answer)
                                     <div class="col-lg-3 col-sm-6 text-center answer-item">
