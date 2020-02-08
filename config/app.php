41212d206282f2b265bd3ddd9dda5b23a93ad2ab
Diego Rodríguez <diego@maniak.com.mx>
2020-02-08 13:18:28 -0600

    Change student url config to app config instead of env

--- config/app.php
+++ config/app.php
@@ -52,17 +52,19 @@ return [
     */
 
     'url' => env('APP_URL', 'http://localhost'),
+    'student_url' => env('STUDENT_APP_URL', 'students.gobrightfox.com'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Application Timezone
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify the default timezone for your application, which
-    | will be used by the PHP date and date-time functions. We have gone
-    | ahead and set this to a sensible default for you out of the box.
-    |
-    */
+
+        /*
+        |--------------------------------------------------------------------------
+        | Application Timezone
+        |--------------------------------------------------------------------------
+        |
+        | Here you may specify the default timezone for your application, which
+        | will be used by the PHP date and date-time functions. We have gone
+        | ahead and set this to a sensible default for you out of the box.
+        |
+        */
 
     'timezone' => 'America/Los_Angeles',
 
